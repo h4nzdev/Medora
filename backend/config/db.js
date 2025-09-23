3c0b51ea26c77d908a8d6980552037445508a47b
Cmhanz99 <hanzhmagbal@gmail.com>
2025-09-23 21:17:35 +0800

    .

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,13 +5,18 @@ if (process.env.NODE_ENV !== "production") {
   dotenv.config();
 }
 
-export const configDB = () => {
-  mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => {
-      console.log("Connected to the database!");
-    })
-    .catch((error) => {
-      console.error("Error on connecting database", error);
-    });
+export const configDB = async () => {
+  const mongoUrl = process.env.MONGO_URI;
+
+  if (!mongoUrl) {
+    console.error("Mongo URL not found! Check your Render environment variables.");
+    return;
+  }
+
+  try {
+    await mongoose.connect(mongoUrl);
+    console.log("Connected to the database!");
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+  }
 };
