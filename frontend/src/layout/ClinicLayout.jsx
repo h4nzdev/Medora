306e60b0c81533fc0e8e686c3ed2d3eed13c5e93
hanzhmagbal <hanzhmagbal@gmail.com>
2025-09-27 13:24:49 +0000

    Added a clinic mobile preview

--- frontend/src/layout/ClinicLayout.jsx
+++ frontend/src/layout/ClinicLayout.jsx
@@ -1,8 +1,14 @@
 import ClinicHeader from "../components/ClinicComponents/ClinicHeader/ClinicHeader";
 import ClinicSidebar from "../components/ClinicComponents/ClinicSidebar";
+import useResponsive from "../hooks/useResponsive";
+import MobileClinicView from "../page/ClinicPages/MobileClinicView";
 
 const ClinicLayout = ({ children }) => {
-  return (
+  const isMobile = useResponsive();
+
+  return isMobile ? (
+    <MobileClinicView />
+  ) : (
     <div className="flex min-h-screen bg-gradient-to-br from-cyan-50 via-sky-50 to-blue-100">
       <ClinicSidebar />
       <div className="flex-1 md:ml-64 lg:ml-64">
